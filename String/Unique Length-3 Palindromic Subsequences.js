494f5db271aab27fe0f77f4f5c128896b1ba92c3
Joy Acharya <joycseuiu@gmail.com>
2023-11-15 01:34:42 +0600

    Update Unique Length-3 Palindromic Subsequences.js

--- String/Unique Length-3 Palindromic Subsequences.js
+++ String/Unique Length-3 Palindromic Subsequences.js
@@ -44,3 +44,46 @@ var countPalindromicSubsequence = function(s) {
     return result;
     
 };
+
+
+
+
+
+
+
+/* approach 2 */
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countPalindromicSubsequence = function(s) {
+    const n = s.length;
+    let result = 0;
+    
+    let map = new Array(26).fill().map(()=>[-1,-1])
+    
+   
+    for(let i = 0; i < n; i++) {
+        let ch = s[i];
+        let idx = ch.charCodeAt(0) - 'a'.charCodeAt(0);
+        if(map[idx][0] == -1) {
+            map[idx][0] = i;
+        }
+        map[idx][1] = i;
+    }
+    
+   for(let i = 0; i < 26; i++) {
+       if(map[i][0] == -1)
+           continue;
+       let st = new Set();
+       
+       for(let m = map[i][0] + 1; m <= map[i][1] -1; m++) {
+           st.add(s[m]);
+       }
+       result+= st.size;
+   } 
+   
+    return result;
+};
